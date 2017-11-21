2d950066b7c78012cee2040fd6b31036d9ecd801
Amir Patel <amirpatel89@gmail.com>
2017-11-21 15:56:03 -0500

    intro to react

---
+++ scripts.jsx
@@ -0,0 +1,24 @@
+function Application(){
+	return(
+		<div id="parent">
+			<div class="sibling">
+				<li>Jennifer</li>
+				<li>Nikolas</li>
+				<li>Zach</li>
+			</div>
+		</div>
+		)
+}
+
+
+
+
+
+
+ReactDOM.render(
+	<Application />,
+	document.getElementById('container')
+	)
+
+
+console.log(React);
